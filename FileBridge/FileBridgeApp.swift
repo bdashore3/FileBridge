ccadaa4562828951dd12a7d87dc92fcf356324af
Brian Dashore <bdashore3@gmail.com>
2021-03-31 16:39:16 -0400

    Initial Commit

---
+++ FileBridge/FileBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileBridgeApp.swift
+//  FileBridge
+//
+//  Created by Brian Dashore on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct FileBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
