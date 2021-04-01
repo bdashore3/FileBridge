b5795c3ef1bcac4086cdb31143499dced3d0e43e
kingbri <bdashore3@gmail.com>
2021-04-01 11:55:25 -0400

    Add file sharing perms
    Also make a very basic UI
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- FileBridge/ContentView.swift
+++ FileBridge/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Welcome to FileBridge")
+                .font(.largeTitle)
+                .padding()
+            VStack (alignment: .center, spacing: 20) {
+                Text("You can close this app")
+                Text("Open your device in iTunes and navigate to File Sharing")
+                    .multilineTextAlignment(.center)
+            }
+            .font(.title2)
             .padding()
+        }
     }
 }
 
